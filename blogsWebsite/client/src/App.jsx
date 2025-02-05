87667a03e1d700cafefc9dbd3a1ebfa56e593908
Dipak Singh <dipaksingh3323@gmail.com>
2025-02-05 10:21:33 +0530

    first Commit

---
+++ blogsWebsite/client/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline bg-red-700 m-10 p-10">
+        Hello world!
+      </h1>
+    </>
+  );
+}
+
+export default App;
